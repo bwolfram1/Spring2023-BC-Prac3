78c42953e86ae7533ebfa2f6ced4007a92389423
bwolfram1 <brandonmwolfram@outlook.com>
2023-04-22 16:43:47 -0400

    rebuilt for deno try #13945

--- .svelte-kit/output/server/index.js
+++ .svelte-kit/output/server/index.js
@@ -2462,7 +2462,7 @@ async function respond(request, options2, manifest, state) {
     fetch: null,
     getClientAddress: state.getClientAddress || (() => {
       throw new Error(
-        `${"@sveltejs/adapter-vercel"} does not specify getClientAddress. Please raise an issue`
+        `${"svelte-adapter-deno"} does not specify getClientAddress. Please raise an issue`
       );
     }),
     locals: {},
--- build/server/chunks/0-c8adf45a.js
+++ build/server/chunks/0-c8adf45a.js
@@ -1,16 +1,13 @@
-const ssr = false;
-
 var _layout = /*#__PURE__*/Object.freeze({
-  __proto__: null,
-  ssr: ssr
+	__proto__: null
 });
 
 const index = 0;
 const component = async () => (await import('./_layout.svelte-2b637b83.js')).default;
 const universal_id = "src/routes/+layout.js";
-const imports = ["_app/immutable/entry/_layout.svelte.70ddb7cd.js","_app/immutable/chunks/index.5caf4ef9.js","_app/immutable/chunks/g80.0caa2d3c.js","_app/immutable/chunks/index.162afc37.js","_app/immutable/entry/_layout.js.7dbd68f0.js","_app/immutable/chunks/_layout.e4a84b88.js"];
+const imports = ["_app/immutable/entry/_layout.svelte.70ddb7cd.js","_app/immutable/chunks/index.5caf4ef9.js","_app/immutable/chunks/g80.0caa2d3c.js","_app/immutable/chunks/index.162afc37.js","_app/immutable/entry/_layout.js.4ed993c7.js"];
 const stylesheets = ["_app/immutable/assets/styles.1a75ed66.css","_app/immutable/assets/g80.9f220f9d.css"];
 const fonts = [];
 
 export { component, fonts, imports, index, stylesheets, _layout as universal, universal_id };
-//# sourceMappingURL=0-b7a6e31c.js.map
+//# sourceMappingURL=0-c8adf45a.js.map
